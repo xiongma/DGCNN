25ed509621296830b2b137f12dcdcec4cc8be6c1
13904 <maxiong@juexiaotime.com>
2019-07-09 09:32:04 +0800

    modify dataset shape and padded

--- data_load.py
+++ data_load.py
@@ -12,12 +12,12 @@ from utils import calc_num_batches
 
 __all__ = ['get_batch', 'load_vocab']
 
-def _load_data(fpath, maxlen1, maxlne2):
+def _load_data(fpath, maxlen1, maxlen2):
     """
     load data
     :param fpath: data path
     :param maxlen1: question max length
-    :param maxlne2: evidence max length
+    :param maxlen2: evidence max length
     :return: questions, list, every element is question
              evidences, list, every element is evidence
              labels, list, every element is list which contain label, start index, end index
@@ -29,13 +29,14 @@ def _load_data(fpath, maxlen1, maxlne2):
     for data in df:
         question = data[1]
         evidence = data[0]
-        if len(question) > maxlen1 or len(evidence) > maxlne2:
-            continue
+        if len(question) > maxlen1: continue
+        if len(evidence) > maxlen2: continue
+
         label = data[3]
         start = data[4]
         end = data[5]
-        questions.append(question)
-        evidences.append(evidence)
+        questions.append(question.encode('utf-8'))
+        evidences.append(evidence.encode('utf-8'))
         labels.append([label, start, end])
 
     return questions, evidences, labels
@@ -70,8 +71,8 @@ def _generator_fn(questions, evidences, labels, vocab_fpath):
     """
     token2idx, _ = load_vocab(vocab_fpath)
     for question, evidence, label in zip(questions, evidences, labels):
-        tokens1 = [token2idx.get(token, token2idx['<unk>']) for token in list(question)]
-        tokens2 = [token2idx.get(token, token2idx['<unk>']) for token in list(evidence)]
+        tokens1 = [token2idx.get(token, token2idx['<unk>']) for token in list(question.decode('utf-8'))]
+        tokens2 = [token2idx.get(token, token2idx['<unk>']) for token in list(evidence.decode('utf-8'))]
 
         yield tokens1, tokens2, label
 
@@ -87,21 +88,25 @@ def _input_fn(questions, evidences, labels, vocab_fpath, batch_size, gpu_nums, m
     :param maxlen1: question max length, int
     :param maxlen2: evidence max length, int
     :param shuffle: whether shuffle data, When train model, it's True
-    :return:
+    :return: tensorflow dataset
     """
-    shapes = ([maxlen1]), ([maxlen2]), ([3])
-    types = ((tf.int32), (tf.int32), (tf.int32))
-    paddings = ((0), (0), (0))
+    shapes = ([None]), ([None]), ([None])
+    padded_shapes = ([maxlen1]), ([maxlen2]), ([3])
+    types = (tf.int32), (tf.int32), (tf.int32)
+    paddings = (0), (0), (0)
     dataset = tf.data.Dataset.from_generator(generator=_generator_fn,
                                              output_shapes=shapes,
                                              output_types=types,
                                              args=(questions, evidences, labels, vocab_fpath))
 
+    # when training, the dataset will be shuffle
     if shuffle:
         dataset = dataset.shuffle(128*batch_size*gpu_nums)
 
     dataset = dataset.repeat() # iterator forever
-    dataset = dataset.padded_batch(batch_size*gpu_nums, shapes, paddings).prefetch(1)
+    dataset = dataset.padded_batch(batch_size=batch_size*gpu_nums,
+                                   padded_shapes=padded_shapes,
+                                   padding_values=paddings).prefetch(1) # padding and batch
 
     return dataset
 
@@ -122,6 +127,11 @@ def get_batch(fpath, maxlen1, maxlen2, vocab_fpath, batch_size, gpu_nums, shuffl
     num_samples
     """
     questions, evidences, labels = _load_data(fpath, maxlen1, maxlen2)
+    # for question in questions:
+    #     if len(question) > maxlen1:
+    #         print(1111)
+    # for evidence in evidences:
+    #     if len(evidence) > maxlen2: print(2222)
     batches = _input_fn(questions, evidences, labels, vocab_fpath, batch_size, gpu_nums, maxlen1, maxlen2, shuffle=shuffle)
     num_batches = calc_num_batches(len(questions), batch_size * gpu_nums)
     return batches, num_batches, len(questions)
