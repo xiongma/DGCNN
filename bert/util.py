24342f8b2ba4a4f98a987c1fbe8c64c1fcd1cc69
13904 <maxiong@juexiaotime.com>
2019-07-19 09:06:10 +0800

    add bert original file

---
+++ bert/util.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+date: 2019/7/11
+mail: cally.maxiong@gmail.com
+blog: http://www.cnblogs.com/callyblog/
+'''
+
+import tensorflow as tf
+
+def label_smoothing(inputs, epsilon=0.1):
+    '''Applies label smoothing. See 5.4 and https://arxiv.org/abs/1512.00567.
+    inputs: 3d tensor. [N, T, V], where V is the number of vocabulary.
+    epsilon: Smoothing rate.
+
+    For example,
+
+    ```
+    import tensorflow as tf
+    inputs = tf.convert_to_tensor([[[0, 0, 1],
+       [0, 1, 0],
+       [1, 0, 0]],
+
+      [[1, 0, 0],
+       [1, 0, 0],
+       [0, 1, 0]]], tf.float32)
+
+    outputs = label_smoothing(inputs)
+
+    with tf.Session() as sess:
+        print(sess.run([outputs]))
+
+    >>
+    [array([[[ 0.03333334,  0.03333334,  0.93333334],
+        [ 0.03333334,  0.93333334,  0.03333334],
+        [ 0.93333334,  0.03333334,  0.03333334]],
+
+       [[ 0.93333334,  0.03333334,  0.03333334],
+        [ 0.93333334,  0.03333334,  0.03333334],
+        [ 0.03333334,  0.93333334,  0.03333334]]], dtype=float32)]
+    ```
+    '''
+    V = tf.cast(tf.shape(inputs)[-1], tf.float32)  # number of channels
+    return ((1 - epsilon) * inputs) + (epsilon / V)
